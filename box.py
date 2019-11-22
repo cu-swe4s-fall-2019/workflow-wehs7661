6da12719575b89fd283d93cfecfcc1cda762142e
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2019-11-21 23:31:04 -0700

    Developed box.py and its corresponding unittest using TDD

---
+++ box.py
@@ -0,0 +1,105 @@
+import os
+import sys
+import argparse
+import matplotlib.pyplot as plt 
+from matplotlib import rc
+
+def initialize():
+    """
+    An argument parser as an initializing function
+    """
+
+    parser = argparse.ArgumentParser(
+        description='This function plots the gene expression distribution \
+                    across a set of genes for a set of tissue group, given \
+                    the set of tissue groups, the set of genes, and the \
+                    name of the output file.')
+    parser.add_argument('-g',
+                        '--gene',
+                        nargs='+',
+                        type=str,
+                        help='The name of the target gene.',
+                        required=True)
+    parser.add_argument('-t',
+                        '--tissue',
+                        nargs='+',
+                        type=str,
+                        help='The name of the tissue group')
+    parser.add_argument('-o',
+                        '--output',
+                        type=str,
+                        help='The file name of the output file.',
+                        required=True)
+
+    args_parse = parser.parse_args()
+
+    return args_parse
+
+def boxplot(data, meta_file, y_label, title, output):
+    plt.subplots(nrows=len(data), figsize=(10, 20))
+    for i in range(len(data)):
+        plt.subplot(len(data), 1, i + 1)
+        plt.boxplot(data[i])
+        plt.ylabel(y_label)
+        plt.title(title[i])
+        plt.xticks(range(1, len(meta_file) + 1), meta_file)
+
+        if i == len(data) - 1:
+            plt.xlabel('Gene')
+
+    plt.savefig(output)
+
+def main():
+    args = initialize()
+
+    rc('font', **{
+        'family': 'sans-serif',
+        'sans-serif': ['DejaVu Sans'],
+        'size': 10
+    })
+    # Set the font used for MathJax - more on this later
+    rc('mathtext', **{'default': 'regular'})
+    plt.rc('font', family='serif')
+
+    tissue_samples_txt, gene_counts_txt = [], []
+
+    for i in range(len(args.tissue)):
+        tissue_samples_txt.append('%s_samples.txt' % args.tissue[i])
+        if not os.path.exists(tissue_samples_txt[i]):
+            print('Input dataset (tissue_samples.txt) not found!')
+            sys.exit(1)
+
+    for i in range(len(args.gene)):
+        gene_counts_txt.append('%s_counts.txt' % args.gene[i])
+        if not os.path.exists(gene_counts_txt[i]):
+            print('Input dataset (gene_counts.txt) not found!')
+            sys.exit(1)
+
+    sample_dist = {}   # (ID, tissue_group) pair
+    gene_dist = {}     # (ID, counts) pair
+    
+    data_all = []
+    for i in range(len(args.tissue)):
+        tissue_id = []
+        tissue_data = open(tissue_samples_txt[i])
+        for l in tissue_data:
+            tissue_id.append(l.split()[0])
+            sample_dist[l.split()[0]] = args.tissue[i]
+
+        counts_all = []
+        for j in range(len(args.gene)):
+            gene_id = []
+            counts = []
+            gene_data = open(gene_counts_txt[j])
+            for l in gene_data:
+                gene_id.append(l.split()[0])
+                gene_dist[l.split()[0]] = int(l.split()[1])
+            intersection_id = [id for id in tissue_id if id in gene_id]
+            counts = [gene_dist[intersection_id[k]] for k in range(len(intersection_id))]
+            counts_all.append(counts)    # counts of each gene type
+        data_all.append(counts_all)     
+
+    boxplot(data_all, args.gene, 'Count', args.tissue, args.output)
+
+if __name__ == '__main__':
+    main()
---
+++ test_box.py
@@ -0,0 +1,17 @@
+import unittest
+import random
+import os
+import box
+
+class TestBox(unittest.TestCase):
+    def test_boxplot(self):
+        data = [[[1, 2, 3, 4, 5]]]
+        meta = ['A', 'B', 'C']
+        title = 'test_boxplot'
+        self.assertFalse(os.path.exists('test.png'))
+        box.boxplot(data, meta, 'y-axis', 'test_boxplot', 'test.png')
+        self.assertTrue(os.path.exists('test.png'))
+        os.remove('test.png')
+
+if __name__ == '__main__':
+    unittest.main()
